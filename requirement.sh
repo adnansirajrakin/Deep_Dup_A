c7d61ebafcbe4e082922caea4bd785c529afdbec
asrakin <asrakin@asu.edu>
2021-05-01 23:36:34 -0700

    Added requirement

---
+++ requirement.sh
@@ -0,0 +1,8 @@
+# bash File 
+# create a conda enviroment: conda create -n dup python=3.6
+# activate: source activate dup
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+pip install scipy
+pip install xlwt
+pip install pandas
+
